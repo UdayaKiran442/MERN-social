d95adc8d1d62e4994d71b8f0ed8db4fcacbe59b5
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-06 15:09:40 +0530

    Handling login input

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -2,20 +2,38 @@ import React from 'react';
 import './Login.css'
 import { Typography,Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const loginHandler = (e) =>{
+        e.preventDefault();
+    }
     return ( 
         <div className='login'>
-            <form className='loginForm'>
+            <form className='loginForm' onSubmit={loginHandler}>
                 <Typography variant='h3' style={{padding:'2vmax'}} >Social App</Typography>
-                <input type='email' placeholder='enter your email' />
-                <input type='password' placeholder='enter your passowrd' />
+                <input 
+                    type='email' 
+                    placeholder='enter your email' 
+                    required 
+                    value={email} 
+                    onChange={(e)=>setEmail(e.target.value)} 
+                />
+                <input 
+                    type='password' 
+                    placeholder='enter your passowrd' 
+                    required 
+                    value={password} 
+                    onChange={(e)=>setPassword(e.target.value)}  
+                />
                 <Link to='/forgot/password'>
                     <Typography>Forgot Password</Typography>
                 </Link>
                 <Link to='/register'>
                     <Typography>New User</Typography>
                 </Link>
-                <Button>Login</Button>
+                <Button type='submit'>Login</Button>
             </form>
         </div>
      );
