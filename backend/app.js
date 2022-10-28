10c5e1ccf618ebd3d9f3224db0301b5039441a95
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-28 11:39:17 +0530

    Login page

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const { urlencoded } = require('express');
 const express = require('express');
 const app = express()
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
 //using middlewares
 app.use(express.json());
 app.use(urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors());
 //using routes
 app.use('/api/v1',require('./routes/post'));
 app.use('/api/v1',require('./routes/user'));
--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import './Login.css'
-import { Typography,Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React from "react";
+import "./Login.css";
+import { Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 const Login = () => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const loginHandler = (e) =>{
-        e.preventDefault();
-    }
-    return ( 
-        <div className='login'>
-            <form className='loginForm' onSubmit={loginHandler}>
-                <Typography variant='h3' style={{padding:'2vmax'}} >Social App</Typography>
-                <input 
-                    type='email' 
-                    placeholder='enter your email' 
-                    required 
-                    value={email} 
-                    onChange={(e)=>setEmail(e.target.value)} 
-                />
-                <input 
-                    type='password' 
-                    placeholder='enter your passowrd' 
-                    required 
-                    value={password} 
-                    onChange={(e)=>setPassword(e.target.value)}  
-                />
-                <Link to='/forgot/password'>
-                    <Typography>Forgot Password</Typography>
-                </Link>
-                <Link to='/register'>
-                    <Typography>New User</Typography>
-                </Link>
-                <Button type='submit'>Login</Button>
-            </form>
-        </div>
-     );
-}
- 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login">
+      <form className="loginForm">
+        <Typography variant="h3" style={{ padding: "2vmax" }}>
+          Social App
+        </Typography>
+        <input
+          type="email"
+          placeholder="enter your email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="enter your passowrd"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Link to="/forgot/password">
+          <Typography>Forgot Password</Typography>
+        </Link>
+        <Link to="/register">
+          <Typography>New User</Typography>
+        </Link>
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
