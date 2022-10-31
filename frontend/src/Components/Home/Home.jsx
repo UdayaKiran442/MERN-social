40e696f383ee51d66ab223ed57a9795ec0735345
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-31 12:30:34 +0530

    Post UI

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Post from "../Post/Post";
 import User from "../User/User";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="home">
-      <div className="homeleft"></div>
+      <div className="homeleft">
+        <Post
+          caption={"This is sample post"}
+          ownerImage={
+            "https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
+          }
+          ownerName={"codewithme"}
+        />
+      </div>
       <div className="homeright">
         <User
           userId={"userId"}
---
+++ frontend/src/Components/Post/Post.jsx
@@ -0,0 +1,82 @@
+import { Avatar, Button, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Post.css";
+import {
+  MoreVert,
+  Favorite,
+  FavoriteBorder,
+  ChatBubbleOutline,
+  DeleteOutline,
+} from "@mui/icons-material";
+import { useState } from "react";
+const Post = ({
+  postId,
+  caption,
+  postImage,
+  likes = [],
+  comments = [],
+  ownerImage,
+  ownerName,
+  ownerId,
+  isDelete = false,
+  isAccount = false,
+}) => {
+  const [liked, setLiked] = useState(false);
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+  return (
+    <div className="post">
+      <div className="postHeader">
+        {isAccount ? (
+          <Button>
+            <MoreVert />
+          </Button>
+        ) : null}
+      </div>
+      <img src={postImage} alt="Post" srcset="" />
+      <div className="postDetails">
+        <Avatar
+          src={ownerImage}
+          alt="User"
+          sx={{
+            height: "3vmax",
+            width: "3vmax",
+          }}
+        />
+        <Link to={`/user/${ownerId}`}>
+          <Typography fontWeight={700}>{ownerName}</Typography>
+        </Link>
+        <Typography
+          fontWeight={100}
+          color="rgba(0,0,0,0.582)"
+          style={{ alignSelf: "center" }}
+        >
+          {caption}
+        </Typography>
+      </div>
+      <button
+        style={{
+          border: "none",
+          backgroundColor: "white",
+          cursor: "pointer",
+          margin: "1vmax 2vmax",
+        }}
+      >
+        <Typography>5 likes</Typography>
+      </button>
+      <div className="postFooter">
+        <Button onClick={handleLike}>
+          {liked ? <Favorite style={{ color: "red" }} /> : <FavoriteBorder />}
+        </Button>
+        <Button>
+          <ChatBubbleOutline />
+        </Button>
+        <Button>{isDelete ? <DeleteOutline /> : null}</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
