ee2e75c23ecc79cfeafee9c579398f4ebbd4970b
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-11-01 09:28:07 +0530

    Fetching posts from API

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,35 +1,56 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Post from "../Post/Post";
 import User from "../User/User";
 import "./Home.css";
 import { getFollowingPosts } from "../../Actions/user";
+import Loader from "../Loader/Loader";
+import { Typography } from "@mui/material";
 
 export const Home = () => {
   const dispatch = useDispatch();
+  const { loading, posts, error } = useSelector(
+    (state) => state.postOfFollowing
+  );
   useEffect(() => {
     dispatch(getFollowingPosts());
   }, []);
   return (
-    <div className="home">
-      <div className="homeleft">
-        <Post
-          caption={"This is sample post"}
-          ownerImage={
-            "https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-          }
-          ownerName={"codewithme"}
-        />
-      </div>
-      <div className="homeright">
-        <User
-          userId={"userId"}
-          name={"user.name"}
-          avatar={
-            "https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-          }
-        />
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="home">
+          <div className="homeleft">
+            {posts && posts.length > 0 ? (
+              posts.map((post, index) => (
+                <Post
+                  key={post._id}
+                  postId={post._id}
+                  caption={post.caption}
+                  postImage={post.image.url}
+                  likes={post.likes}
+                  comments={post.comments}
+                  ownerImage={post.owner.avatar.url}
+                  ownerName={post.owner.name}
+                  ownerId={post.owner._id}
+                />
+              ))
+            ) : (
+              <Typography variant="h6">No posts yet</Typography>
+            )}
+          </div>
+          <div className="homeright">
+            <User
+              userId={"userId"}
+              name={"user.name"}
+              avatar={
+                "https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
+              }
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
